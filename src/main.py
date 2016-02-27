1b0f9a786f871a5bbc30162b9135867d7a3844cc
Kishor Bhat <kishorbhat@gmail.com>
2016-02-27 18:25:39 +0530

    Selection and encryption

--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python
 from itertools import permutations
+from PIL import Image
 import hashlib
 
 
 mask = []
+mask_pattern_len = 0
 key = ""
+prev = 0
 n = 0
 k = 0
 
@@ -24,7 +27,7 @@ def getParams():
     keyInput = raw_input("Enter key:\n> ")
     m = hashlib.md5()
     m.update(keyInput)
-    key = m.hexdigest()
+    key = m.digest()
     nInput = int(raw_input("Enter n:\n> "))
     kInput = int(raw_input("Enter k:\n> "))
     return key, nInput, kInput
@@ -55,10 +58,51 @@ def mask_generator(n, k):
     return mask_pattern_len
 
 
+# plain is a 1D matrix (row-wise read of image matrix)
+def encipher(plain, height, width):
+    global n, key, mask, mask_pattern_len, prev
+    PS = 0
+    shares = [[] for i in range(n)]
+    L = height * width
+    Index = [0 for j in range(L)]
+    for j in range(L):
+        PS += (ord(key[j % 16]) * ord(key[(j+1) % 16]))
+        PS %= L
+        while Index[PS] != 0:
+            PS = (PS+1) % L
+        secretByte = plain[PS]
+        Index[PS] = 1
+        for i in range(n):
+            if (mask[i][j % mask_pattern_len] == '1'):
+                R = (prev ^ (secretByte * ord(key[j % 16]))) % 251
+                shares[i].append(R)
+                prev = secretByte
+    print shares
+
+
+def getImageMatrix(path):
+    im = Image.open(path)
+    pix = im.load()
+    red = []
+    green = []
+    blue = []
+    width, height = im.size
+    for x in range(width):
+        for y in range(height):
+            R, G, B, A = pix[x, y]
+            red.append(R)
+            green.append(G)
+            blue.append(B)
+    return red, green, blue, height, width
+
+
 def run():
+    global key, n, k, mask_pattern_len
     key, n, k = getParams()
-    mask_generator(n, k)
-    print mask
+    mask_pattern_len = mask_generator(n, k)
+    red, green, blue, h, w = getImageMatrix("/home/kishor/pictures/test.png")
+    # print mask
+    encipher(red, h, w)
 
 if __name__ == "__main__":
     run()
