ed2b414c417f581515f6a66e79fec1c81809fd0d
Kishor Bhat <kishorbhat@gmail.com>
2016-02-28 21:03:24 +0530

    Clean up and documentation

--- src/main.py
+++ src/main.py
@@ -13,6 +13,15 @@ n = 0
 k = 0
 
 
+'''
+Arguments:
+    None
+Return values:
+    key, n, k
+Description:
+    Obtains encryption key, n and k parameters from user.
+    Generates MD5(key) and stores this value for further use.
+'''
 def getParams():
     keyInput = raw_input("Enter key:\n> ")
     m = hashlib.md5()
@@ -23,6 +32,17 @@ def getParams():
     return key, nInput, kInput
 
 
+'''
+Arguments:
+    Total number of shares (n), threshold number (k)
+Return values:
+    Length of bit mask.
+Description:
+    Generates all permutations of size n, having
+    * k-1 zeroes
+    * n-k+1 ones
+    These serve as the masks for n shares
+'''
 def mask_generator(n, k):
     global mask
     toPermute = "0"*(k-1) + "1"*(n-k+1)
@@ -38,7 +58,19 @@ def mask_generator(n, k):
     return mask_pattern_len
 
 
-# plain is a 1D matrix (row-wise read of image matrix)
+'''
+Arguments:
+    Secret image in the form of a 1D array, height and width
+    Return values:
+        shares, a list of n shares without the corresponding header information
+    Description:
+        Selects bytes from image and encrypts them according to
+        R = (Pi-1) ^ [(Pi) * K[j%16]) mod 251
+        where:
+            * R = encrypted byte
+            * P = original chosen byte [(Pi-1) = 0]
+            * K = key
+'''
 def encipher(plain, height, width):
     global n, key, mask, mask_pattern_len, prev
     PS = 0
@@ -60,6 +92,15 @@ def encipher(plain, height, width):
     return shares
 
 
+'''
+Arguments:
+    Path to image file to be shared.
+Return values:
+    Height, width, and 1D arrays corresponding to the R,G,B pixel values of
+    each pixel in image.
+Description:
+    Utilizes Python's PIL library to convert given image to pixel matrices.
+'''
 def getImageMatrix(path):
     im = Image.open(path)
     pix = im.load()
@@ -76,27 +117,36 @@ def getImageMatrix(path):
     return red, green, blue, height, width
 
 
+'''
+Arguments:
+    Shares (without header), width of these shares
+Return values:
+    Shares with header information.
+Description:
+    Multiplies shares with header matrix to generate "transmission-ready"
+    shares with header info attached.
+'''
 def addHeader(shares, width):
     global key
     temp = []
-    header_matrix = np.zeros(shape=(n,k))
+    header_matrix = np.zeros(shape=(n, k))
     final_header = [[0 for j in range(k+1)] for i in range(n)]
     for i in range(len(shares)):
         count = 1
         for m in shares[i]:
-            if(m!=0):
-                temp.append(m);
+            if(m != 0):
+                temp.append(m)
                 count = count + 1
             if (count > k):
                 header_matrix[i] = temp
                 temp = []
                 break
-    h = np.zeros(shape=(k,1))
+    h = np.zeros(shape=(k, 1))
     h[0] = n
     h[1] = k
-    for t in range(2,k):
+    for t in range(2, k):
         h[t] = ord(key[t-2])
-    header_middle = np.dot(header_matrix,h)
+    header_middle = np.dot(header_matrix, h)
     for x in range(len(header_middle)):
         binary = "{0:b}".format(int(header_middle[x]))
         binary = "0"*(8*k - len(binary)) + binary
@@ -106,7 +156,7 @@ def addHeader(shares, width):
             binary_split[j/8].append(rev_binary[j])
         for i in range(k):
             binary_split[i] = binary_split[i][::-1]
-            binary_split[i] = int(''.join(binary_split[i]),2)
+            binary_split[i] = int(''.join(binary_split[i]), 2)
         binary_split = binary_split[::-1]
         header_matrix[x] = binary_split
     for i in range(n):
@@ -125,6 +175,15 @@ def addHeader(shares, width):
     return realShares
 
 
+'''
+Arguments:
+    None
+Return values:
+    None
+Description:
+    Serves as a driver function that calls the various components of the
+    implementation.
+'''
 def run():
     global key, n, k, mask_pattern_len
     key, n, k = getParams()
