5f5520d0bcdc270ee1fa697de35bca6bd7c5cb01
Kishor Bhat <kishorbhat@gmail.com>
2016-02-28 19:20:17 +0530

    Add headers to shares

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@
 from itertools import permutations
 from PIL import Image
 import hashlib
+import numpy as np
 
 
 mask = []
@@ -10,17 +11,6 @@ key = ""
 prev = 0
 n = 0
 k = 0
-
-
-class Header():
-
-    def __init__(self):
-        self.shareNumber = -1
-        self.totalShares = n
-        self.thresholdNumber = k
-        self.key = key
-# DUMMY VALUE CHANGE THIS TO WIDTH OF IMAGE
-        self.totalSecretBytes = 42
 
 
 def getParams():
@@ -44,17 +34,7 @@ def mask_generator(n, k):
         mask.append(sortedPerms.pop(0))
     mask = [list(p) for p in mask]
     mask_pattern_len = len(mask)
-    '''
-    for line in mask:
-        print line
-    Transpose the matrix
-    print '================================'
-    '''
     mask = [[line[i] for line in mask] for i in range(n)]
-    '''
-    #for line in mask:
-    #    print line
-    '''
     return mask_pattern_len
 
 
@@ -77,7 +57,7 @@ def encipher(plain, height, width):
                 R = (prev ^ (secretByte * ord(key[j % 16]))) % 251
                 shares[i].append(R)
                 prev = secretByte
-    print shares
+    return shares
 
 
 def getImageMatrix(path):
@@ -96,13 +76,62 @@ def getImageMatrix(path):
     return red, green, blue, height, width
 
 
+def addHeader(shares, width):
+    global key
+    temp = []
+    header_matrix = np.zeros(shape=(n,k))
+    final_header = [[0 for j in range(k+1)] for i in range(n)]
+    for i in range(len(shares)):
+        count = 1
+        for m in shares[i]:
+            if(m!=0):
+                temp.append(m);
+                count = count + 1
+            if (count > k):
+                header_matrix[i] = temp
+                temp = []
+                break
+    h = np.zeros(shape=(k,1))
+    h[0] = n
+    h[1] = k
+    for t in range(2,k):
+        h[t] = ord(key[t-2])
+    header_middle = np.dot(header_matrix,h)
+    for x in range(len(header_middle)):
+        binary = "{0:b}".format(int(header_middle[x]))
+        binary = "0"*(8*k - len(binary)) + binary
+        rev_binary = binary[::-1]
+        binary_split = [[] for i in range(k)]
+        for j in range(len(rev_binary)):
+            binary_split[j/8].append(rev_binary[j])
+        for i in range(k):
+            binary_split[i] = binary_split[i][::-1]
+            binary_split[i] = int(''.join(binary_split[i]),2)
+        binary_split = binary_split[::-1]
+        header_matrix[x] = binary_split
+    for i in range(n):
+        final_header[i] = [i+1] + [int(num) for num in header_matrix[i]]
+    realShares = []
+    for s in shares:
+        realShares.append([s[i:i+width] + [0] for i in xrange(0, len(s), width)])
+    lastPos = len(realShares[0][0]) - 1
+    for idx, s in enumerate(realShares):
+        for jdx, row in enumerate(s):
+            if jdx > k:
+                el = 0
+            else:
+                el = final_header[idx][jdx]
+            realShares[idx][jdx][lastPos] = el
+    return realShares
+
+
 def run():
     global key, n, k, mask_pattern_len
     key, n, k = getParams()
     mask_pattern_len = mask_generator(n, k)
     red, green, blue, h, w = getImageMatrix("/home/kishor/pictures/test.png")
-    # print mask
-    encipher(red, h, w)
+    shares = encipher(red, h, w)
+    shares = addHeader(shares, w)
 
 if __name__ == "__main__":
     run()
